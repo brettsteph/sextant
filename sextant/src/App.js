f01f84b05b0609b4a45478ddeca5cd976c4c0f17
Brett Stephen <igadget777@gmail.com>
2022-08-20 18:30:45 -0400

    Add Banner and Exibit Components and delete code

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Banner from './components/Banner'
+import Exibit from './components/Exibit'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner siteTitle="Sextant" />
+      <Exibit title="Data Points" />
     </div>
   );
 }
