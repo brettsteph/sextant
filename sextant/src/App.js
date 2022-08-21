4f3024612fcb0c3007929e833fde0b8049982675
Brett Stephen <igadget777@gmail.com>
2022-08-20 20:27:39 -0400

    Import PublicIP component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,12 +1,14 @@
 import Banner from './components/Banner'
 import Exibit from './components/Exibit'
+import PublicIP from './components/PublicIP';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Banner siteTitle="Sextant" />
-      <Exibit title="Data Points" />
+      <Exibit title="Public IP4 Address" ipComponent={<PublicIP ipVersion="ipv4"/>} />
+      <Exibit title="Public IP6 Address" ipComponent={<PublicIP ipVersion="ipv6"/>} />
     </div>
   );
 }
