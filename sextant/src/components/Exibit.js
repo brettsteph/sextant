09f2260c24063c7e76d6e84f2181a980c15f233f
Brett Stephen <igadget777@gmail.com>
2022-08-22 14:24:27 -0400

    Refactor code

--- sextant/src/components/Exibit.js
+++ sextant/src/components/Exibit.js
@@ -1,11 +1,11 @@
 import './Exibit.css'
 
 function Exibit(props) {
-  const { title, ipComponent } = props;
+  const { title, Component } = props;
   return (
     <div className="exibit-card">
       <h2 className='exibit-title'>{title}</h2>
-      {ipComponent}
+      {Component}
     </div>
   )
 }
